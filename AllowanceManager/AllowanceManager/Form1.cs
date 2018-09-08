3c388885b4ec0e34d148746cc2efe78bbbe8d634
김동건 <ehdrjs2011@gmail.com>
2018-09-09 02:13:49 +0900

    Allowance Manager C#

---
+++ AllowanceManager/AllowanceManager/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AllowanceManager
+{
+    public partial class AllowanceManager : Form
+    {
+        public AllowanceManager()
+        {
+            InitializeComponent();
+        }
+
+        private void AllowanceManager_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
